f22cde6f141bcc3691ab0ea01177ea2005f0c807
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2024-09-03 13:59:23 +0530

    Add tests

---
+++ DeviantArt.NetTest/ApiClientTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+using DeviantArt.Net.Api;
+using DeviantArt.Net.Client.Authentication;
+using DeviantArt.Net.Models;
+using DeviantArt.Net.Modules.Client.Authentication;
+using DeviantArt.NetTest.Util;
+
+namespace DeviantArt.NetTest
+{
+    [TestClass]
+    public class AuthenticatedDeviantArtApiClientTests
+    {
+        private DeviantArtOAuthClient _oauthClient;
+        private AuthenticatedDeviantArtApiClient _client;
+        
+        
+        private ITokenStore _tokenStore;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            DeviantArtOAuthClient oauthClient = new(Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_ID")!,
+                Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_SECRET")!, new InMemoryTokenStore());
+            _client = new AuthenticatedDeviantArtApiClient(oauthClient);
+        }
+
+        private static T LoadJsonFromFile<T>(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json) ?? throw new InvalidOperationException(filePath);
+        }
+
+        [TestMethod]
+        public async Task isTokenValidAsync_ShouldReturnPlaceboResponse()
+        {
+            
+            var expectedResponse = LoadJsonFromFile<PlaceboResponse>("ExpectedResponses/Auth/PlaceboResponse.json");
+            
+            var result = await _client.IsTokenValidAsync();
+            
+            Assert.AreEqual(expectedResponse.Status, result.Status);
+        }
+
+        [TestMethod]
+        public async Task GetDeviationAsync_ShouldReturnDeviation()
+        {
+            var expectedDeviation = LoadJsonFromFile<Deviation>("ExpectedResponses/Deviations/Deviation.json");
+            
+            var result = await _client.GetDeviationAsync(Ids.PoolByAllRich);
+            
+            Assert.AreEqual(expectedDeviation.DeviationId, result.DeviationId);
+        }
+
+        [TestMethod]
+        public async Task BrowseHomeDeviationsAsync_ShouldReturnDeviantArtApiResponse()
+        {
+            
+            const int limit = 10;
+            const int offset = 0;
+            const bool matureContent = false;
+            
+            var result = await _client.BrowseHomeDeviationsAsync(limit, offset, matureContent);
+            
+            Assert.IsFalse(result.Results.Count > 10);
+        }
+
+        [TestMethod]
+        public async Task BrowseTagsAsync_ShouldReturnBrowseTagsResponse()
+        {
+            
+            const string tag = "art";
+            var expectedResponse = LoadJsonFromFile<BrowseTagsResponse>("ExpectedResponses/Browse/BrowseTags.json");
+            
+            var result = await _client.BrowseTagsAsync(tag);
+            
+            Assert.AreEqual(expectedResponse.Results.Count, result.Results.Count);
+        }
+    }
+}
---
+++ DeviantArt.NetTest/Util/Ids.cs
@@ -0,0 +1,6 @@
+﻿namespace DeviantArt.NetTest.Util;
+
+public static class Ids
+{
+    public const string PoolByAllRich = "4F494EC9-B8E9-CE6B-AC56-5D4ADBEDD8E2";
+}
