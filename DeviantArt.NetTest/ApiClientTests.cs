304d2bd4bdebf55f1ccd84cc090e01db9b12fd58
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2024-09-03 14:53:41 +0530

    reformat

--- DeviantArt.NetTest/ApiClientTests.cs
+++ DeviantArt.NetTest/ApiClientTests.cs
@@ -5,75 +5,79 @@ using DeviantArt.Net.Models;
 using DeviantArt.Net.Modules.Client.Authentication;
 using DeviantArt.NetTest.Util;
 
-namespace DeviantArt.NetTest
+namespace DeviantArt.NetTest;
+
+[TestClass]
+public class AuthenticatedDeviantArtApiClientTests
 {
-    [TestClass]
-    public class AuthenticatedDeviantArtApiClientTests
+    private DeviantArtOAuthClient _oauthClient;
+    private readonly AuthenticatedDeviantArtApiClient _client;
+        
+        
+    private readonly ITokenStore _tokenStore = new InMemoryTokenStore();
+
+    public AuthenticatedDeviantArtApiClientTests()
     {
-        private DeviantArtOAuthClient _oauthClient;
-        private AuthenticatedDeviantArtApiClient _client;
-        
-        
-        private ITokenStore _tokenStore;
+        _oauthClient= new DeviantArtOAuthClient(Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_ID")!,
+            Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_SECRET")!, _tokenStore);
+        _client = new AuthenticatedDeviantArtApiClient(_oauthClient);
+    }
 
-        [TestInitialize]
-        public void Setup()
-        {
-            DeviantArtOAuthClient oauthClient = new(Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_ID")!,
-                Environment.GetEnvironmentVariable("DEVIANT_ART_CLIENT_SECRET")!, new InMemoryTokenStore());
-            _client = new AuthenticatedDeviantArtApiClient(oauthClient);
-        }
+    [TestInitialize]
+    public void Setup()
+    {
+           
+    }
 
-        private static T LoadJsonFromFile<T>(string filePath)
-        {
-            var json = File.ReadAllText(filePath);
-            return JsonSerializer.Deserialize<T>(json) ?? throw new InvalidOperationException(filePath);
-        }
+    private static T LoadJsonFromFile<T>(string filePath)
+    {
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<T>(json) ?? throw new InvalidOperationException(filePath);
+    }
 
-        [TestMethod]
-        public async Task isTokenValidAsync_ShouldReturnPlaceboResponse()
-        {
+    [TestMethod]
+    public async Task isTokenValidAsync_ShouldReturnPlaceboResponse()
+    {
             
-            var expectedResponse = LoadJsonFromFile<PlaceboResponse>("ExpectedResponses/Auth/PlaceboResponse.json");
+        var expectedResponse = LoadJsonFromFile<PlaceboResponse>("ExpectedResponses/Auth/PlaceboResponse.json");
             
-            var result = await _client.IsTokenValidAsync();
+        var result = await _client.IsTokenValidAsync();
             
-            Assert.AreEqual(expectedResponse.Status, result.Status);
-        }
+        Assert.AreEqual(expectedResponse.Status, result.Status);
+    }
 
-        [TestMethod]
-        public async Task GetDeviationAsync_ShouldReturnDeviation()
-        {
-            var expectedDeviation = LoadJsonFromFile<Deviation>("ExpectedResponses/Deviations/Deviation.json");
+    [TestMethod]
+    public async Task GetDeviationAsync_ShouldReturnDeviation()
+    {
+        var expectedDeviation = LoadJsonFromFile<Deviation>("ExpectedResponses/Deviations/Deviation.json");
             
-            var result = await _client.GetDeviationAsync(Ids.PoolByAllRich);
+        var result = await _client.GetDeviationAsync(Ids.PoolByAllRich);
             
-            Assert.AreEqual(expectedDeviation.DeviationId, result.DeviationId);
-        }
+        Assert.AreEqual(expectedDeviation.DeviationId, result.DeviationId);
+    }
 
-        [TestMethod]
-        public async Task BrowseHomeDeviationsAsync_ShouldReturnDeviantArtApiResponse()
-        {
+    [TestMethod]
+    public async Task BrowseHomeDeviationsAsync_ShouldReturnDeviantArtApiResponse()
+    {
             
-            const int limit = 10;
-            const int offset = 0;
-            const bool matureContent = false;
+        const int limit = 10;
+        const int offset = 0;
+        const bool matureContent = false;
             
-            var result = await _client.BrowseHomeDeviationsAsync(limit, offset, matureContent);
+        var result = await _client.BrowseHomeDeviationsAsync(limit, offset, matureContent);
             
-            Assert.IsFalse(result.Results.Count > 10);
-        }
+        Assert.IsFalse(result.Results.Count > 10);
+    }
 
-        [TestMethod]
-        public async Task BrowseTagsAsync_ShouldReturnBrowseTagsResponse()
-        {
+    [TestMethod]
+    public async Task BrowseTagsAsync_ShouldReturnBrowseTagsResponse()
+    {
             
-            const string tag = "art";
-            var expectedResponse = LoadJsonFromFile<BrowseTagsResponse>("ExpectedResponses/Browse/BrowseTags.json");
+        const string tag = "art";
+        var expectedResponse = LoadJsonFromFile<BrowseTagsResponse>("ExpectedResponses/Browse/BrowseTags.json");
             
-            var result = await _client.BrowseTagsAsync(tag);
+        var result = await _client.BrowseTagsAsync(tag);
             
-            Assert.AreEqual(expectedResponse.Results.Count, result.Results.Count);
-        }
+        Assert.AreEqual(expectedResponse.Results.Count, result.Results.Count);
     }
 }
