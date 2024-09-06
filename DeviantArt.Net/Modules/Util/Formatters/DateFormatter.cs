bdd5bc3ee2f0c21dca6b746a46aa7af05e5c18e5
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2024-09-06 13:02:04 +0530

    Make CustomDateUrlParameterFormatter inherit DefaultUrlParameterFormatter

--- DeviantArt.Net/Modules/Util/Formatters/DateFormatter.cs
+++ DeviantArt.Net/Modules/Util/Formatters/DateFormatter.cs
@@ -2,15 +2,15 @@
 
 namespace DeviantArt.Net.Modules.Util.Formatters;
 
-public class CustomDateUrlParameterFormatter : IUrlParameterFormatter
+public class CustomDateUrlParameterFormatter : DefaultUrlParameterFormatter
 {
-    public string? Format(object? value, ICustomAttributeProvider attributeProvider, Type type)
+    public override string? Format(object? value, ICustomAttributeProvider attributeProvider, Type type)
     {
         if (value is DateOnly dt)
         {
             return dt.ToString("yyyy-MM-dd");
         }
 
-        return value?.ToString();
+        return base.Format(value, attributeProvider, type);
     }
 }
